4eba606da231535e575eabc631cfaf80d38f04ca
dima <mogilnikovdmitrij12@gmail.com>
2025-09-19 21:14:42 +0300

    realized help function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
-
+#include <string>
+#include <fstream>
+#include <map>
+#include <functional>
 using namespace std;
 
 int main() {
-    cout << "Welcome! wait comand:"
+    ifstream help("hepl.txt");
+    if (!help.is_open()) {
+        cout << "Fatal! help didn't open \n";
+        return 1;
+    }
+
+    cout << "Welcome! waiting for the command:";
+    map<string, function<void()>> commands;
+
+    
+    string line;
+    commands["help"] = [&help, &line](){
+        help.clear();
+        help.seekg(0);
+        while (getline(help, line)) { 
+        cout << line << endl;      
+        }
+    };
+    string command;
+    while (true)
+    {
+        cout << "> ";
+        getline(cin, command);
+        if (commands.count(command)) {
+            commands[command]();
+        } else {
+            cout << "Unexpected command!\n";
+        }
+    }
+    
+
 }
