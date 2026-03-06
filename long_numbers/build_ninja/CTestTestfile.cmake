65579e947d1b1e4757faa6e395dc4207539c2f59
dima <mogilnikovdmitrij12@gmail.com>
2026-03-07 01:11:56 +0300

    delete build directory

--- long_numbers/build_ninja/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dima/Documents/MPM/homework-for-modern-programming-methods-/long_numbers
-# Build directory: /home/dima/Documents/MPM/homework-for-modern-programming-methods-/long_numbers/build_ninja
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LongNumberTests "/home/dima/Documents/MPM/homework-for-modern-programming-methods-/long_numbers/build_ninja/long_numbers_tests")
-set_tests_properties(LongNumberTests PROPERTIES  _BACKTRACE_TRIPLES "/home/dima/Documents/MPM/homework-for-modern-programming-methods-/long_numbers/CMakeLists.txt;21;add_test;/home/dima/Documents/MPM/homework-for-modern-programming-methods-/long_numbers/CMakeLists.txt;0;")
-subdirs("googletest/googletest")
--- long_numbers/build_ninja/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dima/Documents/MPM/homework-for-modern-programming-methods-/long_numbers/googletest/googletest
-# Build directory: /home/dima/Documents/MPM/homework-for-modern-programming-methods-/long_numbers/build_ninja/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
