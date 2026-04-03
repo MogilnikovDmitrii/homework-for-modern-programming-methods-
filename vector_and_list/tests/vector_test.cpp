0a40f4bc7c63a30bbac5fd9b8392442d0ab6223c
dima <mogilnikovdmitrij12@gmail.com>
2026-04-04 00:26:41 +0300

    deletetips

--- vector_and_list/tests/vector_test.cpp
+++ vector_and_list/tests/vector_test.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include "vector.hpp"
 using DmMog::Vector;
-// Тестируем push_back и get_size
 TEST(VectorTest, PushBackAndSize) {
     Vector<int> v;
     EXPECT_EQ(v.get_size(), 0);
@@ -15,32 +14,26 @@ TEST(VectorTest, PushBackAndSize) {
     EXPECT_TRUE(v.has_item(20));
 }
 
-// Тестируем insert
 TEST(VectorTest, Insert) {
     Vector<int> v;
     v.push_back(1);
     v.push_back(3);
 
-    // Вставка в середину
     EXPECT_TRUE(v.insert(1, 2));
     EXPECT_EQ(v.get_size(), 3);
     EXPECT_TRUE(v.has_item(2));
 
-    // Вставка в начало
     EXPECT_TRUE(v.insert(0, 0));
     EXPECT_EQ(v.get_size(), 4);
     EXPECT_TRUE(v.has_item(0));
 
-    // Вставка в конец
     EXPECT_TRUE(v.insert(4, 4));
     EXPECT_EQ(v.get_size(), 5);
     EXPECT_TRUE(v.has_item(4));
 
-    // Невалидная вставка
     EXPECT_FALSE(v.insert(10, 99));
 }
 
-// Тестируем remove_first
 TEST(VectorTest, RemoveFirst) {
     Vector<int> v;
     v.push_back(1);
@@ -48,12 +41,10 @@ TEST(VectorTest, RemoveFirst) {
     v.push_back(3);
     v.push_back(2);
 
-    // Удаляем первый элемент 2
     EXPECT_TRUE(v.remove_first(2));
     EXPECT_EQ(v.get_size(), 3);
-    EXPECT_TRUE(v.has_item(2)); // второй 2 всё ещё есть
+    EXPECT_TRUE(v.has_item(2)); 
 
-    // Удаляем несуществующий элемент
     EXPECT_FALSE(v.remove_first(99));
 }
 
